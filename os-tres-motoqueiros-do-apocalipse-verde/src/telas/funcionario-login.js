3373a699d0131c9ab4e4ae06793d0c9d624b573c
Erick0105 <erickaxs0105@gmail.com>
2025-05-21 16:38:17 -0300

    COMMITA TA DANDO PAU JA

---
+++ os-tres-motoqueiros-do-apocalipse-verde/src/telas/funcionario-login.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Página de Login do Funcionario</Text>
+    </View>
+  );
+}
